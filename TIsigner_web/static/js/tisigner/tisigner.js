c7304553a1eff0351fd54dad52ce27d0ca26a081
bikash <mk.bikash@gmail.com>
2019-08-19 13:52:59 +1200

    update

--- TIsigner_web/static/js/tisigner/tisigner.js
+++ TIsigner_web/static/js/tisigner/tisigner.js
@@ -195,24 +195,17 @@ $(document)
 $(document).on("click", '#try-again',
 //$('#try-again').on('click',
 function () {
-<<<<<<< HEAD
     //$("#input-form").collapse("show");
     $('#input-form').css('pointer-events', 'auto');
-=======
-    $("#input-form").collapse("show");
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
     $('#submit-btn')[0].disabled = false;
     $('#show-results').collapse("hide");
     $('#show-more').html('Show more');
     $('#infinite-prog-bar').collapse("hide");
     $('#input-form')[0].reset();
     //    $("#input-form").collapse("show");
-<<<<<<< HEAD
     $('#host-help').collapse("show");
     $('#cst-region').collapse("hide");
     $('#custom-region').attr("required", false);
-=======
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
     $('#extra-results').collapse("hide");
     $('#primer-length-selection-slider').collapse("show");
     $('#len-primer').val($('#primer-button').val() - 1);
@@ -235,44 +228,29 @@ $(document).ready(function () {
     //Show custom UTR form if custom is selcted from drop down.
     $('#utr')
         .change(function () {
-<<<<<<< HEAD
             var utrval = $(this).val();
             //console.log('utrchange');
             if (utrval == '1' && $('#host-select').val() === "Escherichia coli") {
                 //console.log('lvl-select');
                 $('#optimisation-type').collapse("hide");
-=======
-            var utrval = $(this).val()
-            //console.log('utrchange');
-            if (utrval == '1') {
-                //console.log('lvl-select');
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
                 $('#cst-utr').collapse("hide");
                 $('#custom-utr').attr("required", false);
                 $("#lvl-selection-slider").collapse("show");
                 $('#lvl-tune-val-txt').val(100);
-<<<<<<< HEAD
 
             } else if (utrval == '3') {
                 $('#optimisation-type').collapse("show");
-=======
-            } else if (utrval == '3') {
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
                 $('#cst-utr').collapse("show");
                 $('#custom-utr').attr("required", true);
                 $("#lvl-selection-slider").collapse("hide")
             } else {
                 //console.log('utrselect');
-<<<<<<< HEAD
                 $('#optimisation-type').collapse("show");
-=======
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
                 $('#cst-utr').collapse("hide");
                 $("#lvl-selection-slider").collapse("hide");
                 $('#custom-utr').attr("required", false);
             }
         });
-<<<<<<< HEAD
 
     $('#host-select').change(function () {
         var host = $(this).val();
@@ -304,10 +282,6 @@ $(document).ready(function () {
 
 //$(document).ready(function(){ });
 
-=======
-});
-
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
 function updateTuning(elem) {
     var level = "High";
     // //console.log($(elem).val()); //console.log(100 >= $(elem).val() &&
@@ -328,15 +302,10 @@ function updateTuning(elem) {
 function hostSelect() {
     $('select[name=host-select]')
         .change(function () {
-<<<<<<< HEAD
             if ($(this).val() !== "Escherichia coli") {
                 $("#lvl-selection-slider").collapse("hide");
             } else if ($(this).val() === "Escherichia coli" && $('#utr').val() === '1') {
                 $("#lvl-selection-slider").collapse("show");
-=======
-            if ($(this).val() == 'Pichia pastoris') {
-                $("#lvl-selection-slider").collapse("hide");
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
             }
         });
 }
@@ -419,7 +388,6 @@ function maketable(elem, data) {
     tbody.appendTo(elem);
 }
 
-<<<<<<< HEAD
 function validateCustomRange(elem) {
     var region = elem.val();
     var reg = region.split(':');
@@ -447,167 +415,6 @@ function validateCustomRange(elem) {
         return true;
     }
 }
-=======
-var defUtr = "GGGGAATTGTGAGCGGATAACAATTCCCCTCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACAT"; //default utr
-var points = [
-    0,
-    0.67346939,
-    1.34693878,
-    2.02040816,
-    2.69387755,
-    3.36734694,
-    4.04081633,
-    4.71428571,
-    5.3877551,
-    6.06122449,
-    6.73469388,
-    7.40816327,
-    8.08163265,
-    8.75510204,
-    9.42857143,
-    10.10204082,
-    10.7755102,
-    11.44897959,
-    12.12244898,
-    12.79591837,
-    13.46938776,
-    14.14285714,
-    14.81632653,
-    15.48979592,
-    16.16326531,
-    16.83673469,
-    17.51020408,
-    18.18367347,
-    18.85714286,
-    19.53061224,
-    20.20408163,
-    20.87755102,
-    21.55102041,
-    22.2244898,
-    22.89795918,
-    23.57142857,
-    24.24489796,
-    24.91836735,
-    25.59183673,
-    26.26530612,
-    26.93877551,
-    27.6122449,
-    28.28571429,
-    28.95918367,
-    29.63265306,
-    30.30612245,
-    30.97959184,
-    31.65306122,
-    32.32653061,
-    33
-]
-
-var success_openen = [
-    1.98738964346095e-06,
-    1.98738964346095e-06,
-    1.98738964346095e-06,
-    3.979134904699868e-05,
-    0.0005614693463309397,
-    0.0015994783332591886,
-    0.007059343154791341,
-    0.01959674401987233,
-    0.03344511636457853,
-    0.05846219766091034,
-    0.08812439667470215,
-    0.11259352563647178,
-    0.1357682202648517,
-    0.14211606213908856,
-    0.14570074633478247,
-    0.1351020183433137,
-    0.1268336184495585,
-    0.10785947362483832,
-    0.09562433135532121,
-    0.07934998819340157,
-    0.05797529403020855,
-    0.04464152892547112,
-    0.031056912967964322,
-    0.019465426689115016,
-    0.013458606780850584,
-    0.009290589646287092,
-    0.0072191819217718774,
-    0.00481998609914608,
-    0.003381550290010877,
-    0.0027037492134495186,
-    0.0018640790321791344,
-    0.0010672153576768515,
-    0.00030447049761963126,
-    0.0001961151937122446,
-    4.936898440389916e-05,
-    4.358236613730531e-05,
-    0.0002030749922550749,
-    0.00014137135559688042,
-    0.00010345471895537332,
-    4.37357584524152e-05,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06,
-    1.1173963588429985e-06
-]
-
-var failed_openen = [
-    5.437758361148604e-06,
-    5.437758361148604e-06,
-    5.437758361148604e-06,
-    5.397169299206176e-05,
-    0.00032248749940829824,
-    0.0012660911082912243,
-    0.005280751012420593,
-    0.009821077717552978,
-    0.015469945304867552,
-    0.023015782861449723,
-    0.033982478828445295,
-    0.048250752743149404,
-    0.06741365641897708,
-    0.07535156640619728,
-    0.0834311789634686,
-    0.09314100388060208,
-    0.10061604385988879,
-    0.10365480589977134,
-    0.10144817567167914,
-    0.09734173335858409,
-    0.09274376624962825,
-    0.08880318802397255,
-    0.08443827862752538,
-    0.07766982571253733,
-    0.06290949102413919,
-    0.051156199195859016,
-    0.03966004606891997,
-    0.03015605437361742,
-    0.023554815332062107,
-    0.0191747904887778,
-    0.013276183928893279,
-    0.00912923396576851,
-    0.005819565146953443,
-    0.004128214349275207,
-    0.0036718639573756536,
-    0.0034950737504194852,
-    0.002985476454187301,
-    0.0018129676325811912,
-    0.0012868050108287184,
-    0.0009077691932529392,
-    0.0005222456180022826,
-    0.0002047648974706759,
-    6.214594206722606e-05,
-    0.00011287484615139223,
-    0.00019046772982714063,
-    0.00016835234521245699,
-    7.676975792930047e-05,
-    1.805206870461312e-05,
-    2.1888850127133413e-06,
-    2.1888850127133413e-06
-]
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
 
 //$(document).ready(function() {
 function validateinputs(event) {
@@ -627,14 +434,7 @@ function validateinputs(event) {
     $('#utr').val() == '3'
         ? (utrNucl = inpUtr)
         : (utrNucl = defUtr);
-<<<<<<< HEAD
     //console.log(utrNucl);
-=======
-    ////console.log(utrNucl);
-    var showGraph = ($('#utr').val() === '1' && $('#host-select').val() === "Escherichia coli")
-        ? 1
-        : 0;
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
     var reg = /^[ATGCU]*$/;
     var stCdns = ["TAG", "TGA", "TAA"]
     var cdns = inpSeq.match(/.{1,3}/g);
@@ -698,12 +498,6 @@ function validateinputs(event) {
         } else if (customrangevalidatemsg !== true) {
             $('#seq-err-msg').append(customrangevalidatemsg);
         }
-<<<<<<< HEAD
-=======
-
-        // console.log("Stop codons inside open reading frame or unknown nucleotides or
-        // no start codon or wrong RMS format.");
->>>>>>> eff7ec14bcafc0d7e4d41063c9d435cb2551af62
         $('#seq-err').collapse("show");
         event.preventDefault();
 
@@ -940,4 +734,4 @@ function errorfunc(jqXHR, textStatus, errorThrown) {
         $('#input-form-card').append(`<div class='card-body'><h5 class='card-title'>Error ${jqXHR.status}</h5><h6 class='card-subtitle mb-2 text-muted'>${errors}</h6><p class='card-text'>Please refresh the page and try again.</p><div class='container'><canvas id='errorCanvas' style=';width:100%;height:100%'></canvas></div></div>`);
     }
     //console.log('An error occurred.'); console.log(errors);
-
+}
